7ed055ae75eb8b5c8c6b4c596c9a2101a63e670c
jackibolt <40141575+jackibolt@users.noreply.github.com>
2023-12-31 16:25:49 -0500

    most of the app

--- app.py
+++ app.py
@@ -10,6 +10,19 @@ def clean_date(date_str):
     return datetime.date(year, month, 1)
 
 
+def month_string(month):
+    # months = ['January', 'February', 'March', 'April', 
+    #           'May', 'June', 'July', 'August', 'September', 
+    #           'October', 'November', 'December']
+    # str_month = months[month-1]
+    str_month = month.strftime('%B %Y')
+    return str_month
+
+
+def display_month(month):
+    updated_month = month.strftime('%Y-%m')
+    return updated_month
+
 
 @app.route('/')
 def index():
@@ -18,17 +31,19 @@ def index():
     return render_template('index.html', projects=projects)
 
 
-@app.route('/detail/<id>')
+@app.route('/projects/<id>')
 def project_details(id):
-    project = Project.query.get(id)
+    projects = Project.query.all()
+    project = Project.query.get_or_404(id)
+    project.date = month_string(project.date)
 
-    return render_template('detail.html', project=project)
+
+    return render_template('detail.html', project=project, projects=projects)
 
 
-@app.route('/new', methods=['GET', 'POST'])
+@app.route('/projects/new', methods=['GET', 'POST'])
 def new():
-
-    print(request.form)
+    projects = Project.query.all()
 
     if request.form:
         new_project = Project(name=request.form['title'], date=request.form['date'],
@@ -40,17 +55,42 @@ def new():
         print('item committed')
         return redirect( url_for('index'))
 
-    return render_template('projectform.html')
+    return render_template('projectform.html', projects=projects)
 
 
-@app.route('/update/<id>', methods=['GET', 'POST'])
+@app.route('/projects/<id>/edit', methods=['GET', 'POST'])
 def update(id):
+    projects = Project.query.all()
+    project = Project.query.get_or_404(id)
+
+    if request.form:
+        project.name = request.form['title']
+        project.date = clean_date(request.form['date'])
+        project.description = request.form['desc']
+        project.skills = request.form['skills']
+        project.link = request.form['github']
+        db.session.commit()
+
+        return redirect( url_for('index') )
+
+    project.date = display_month(project.date)
 
 
-    return render_template('editform.html')
+    return render_template('editform.html', project=project, projects=projects)
+
+
+@app.route('/projects/<id>/delete')
+def delete_project(id):
+    project = Project.query.get_or_404(id)
+    db.session.delete(project)
+    db.session.commit()
+
+    return redirect( url_for('index') )
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=5500, host='127.0.0.1')
+
+        
